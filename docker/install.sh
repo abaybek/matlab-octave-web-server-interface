530e67a42ab21bf4df982622c4db65415db214ed
Abay Bek <bekbaganbetov.abay@gmail.com>
2018-03-12 16:56:31 +0600

    Dockerfile fix

--- docker/install.sh
+++ docker/install.sh
@@ -3,17 +3,4 @@
 # exit if a command fails
 set -e
 
-apt-get update
-apt-get install -y software-properties-common
-apt-get install -y octave vim git default-jre
-apt-get remove -y software-properties-common
-
-
-# cleanup package manager
-apt-get autoclean && apt-get clean
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# prepare dir
-mkdir /source
-cd /source
-git clone https://github.com/precisesimulation/matlab-octave-web-server-interface.git .
+octave --eval="web_server('start')" --persist
